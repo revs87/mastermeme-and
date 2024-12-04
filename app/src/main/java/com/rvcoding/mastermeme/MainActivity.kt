a14c2999743e1f1b8512cad618633d52c24c0d27
Rui Vieira <revs87@msn.com>
2024-12-04 17:28:31 +0100

    Project setup

---
+++ app/src/main/java/com/rvcoding/mastermeme/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.rvcoding.mastermeme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.tooling.preview.Preview
+import com.rvcoding.mastermeme.ui.theme.Background
+import com.rvcoding.mastermeme.ui.theme.MasterMemeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Background.toArgb(),
+                darkScrim = Background.toArgb()
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Background.toArgb(),
+                darkScrim = Background.toArgb()
+            )
+        )
+        setContent {
+            MasterMemeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MasterMemeTheme {
+        Greeting("Android")
+    }
+}
